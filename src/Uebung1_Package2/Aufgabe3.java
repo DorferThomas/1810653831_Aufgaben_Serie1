7a171400f91c4587d622b185243c0c03129c10d8
Dorfer Thomas Josef <1810653831@stud.fh-kufstein.ac.at>
2018-11-08 14:17:44 +0100

    Aufgabe3ZweiVariablenMitUnterschiedlichenTypenAngelegt+Ausgegeben

---
+++ src/Uebung1_Package2/Aufgabe3.java
@@ -0,0 +1,14 @@
+package Uebung1_Package2;
+
+public class Aufgabe3 {
+    public static void main(String[] args)
+    {
+        //Zwei Variablen mit unterschiedlichen Typen erstellt und jeder einen Wert hinzugefügt.
+        String a = "Hallo";
+        int b = 24;
+
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+}
