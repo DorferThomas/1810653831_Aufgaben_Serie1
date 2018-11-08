14fc3758878fa8877818679a5234fd5765ad6be3
Dorfer Thomas Josef <1810653831@stud.fh-kufstein.ac.at>
2018-11-08 14:10:10 +0100

    Aufgabe2KommentareFürJedeZeileHinzugefügt

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+//Main Klasse wird aufgerufen
+public class Main {
+
+    //Die Main Methode dieser Klasse wird aufgerufen
+    public static void main(String[] args)
+    {
+        //Das System gibt auf der Konsole Thomas Dorfer aus.
+        System.out.println("Thomas Dorfer");
+    }
+}
--- src/Uebung1_Package1/Aufgabe1.java
+++ src/Uebung1_Package1/Aufgabe1.java
@@ -1,7 +1,13 @@
+//Packet Uebung1_Package1 wird geöffnet (Mit rechtsklick auf src kann man es erstellen)
 package Uebung1_Package1;
 
+//Die Klasse Aufgabe1 wurde erstellt und wird hier aufgerufen
 public class Aufgabe1 {
+
+    //Die Main Methode dieser Klasse wird aufgerufen
     public static void main(String[] args){
+
+        //Das System gibt FH Kufstein Tirol auf der Konsole aus.
         System.out.println("FH Kufstein Tirol");
     }
 }
