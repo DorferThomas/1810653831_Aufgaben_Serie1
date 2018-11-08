5257dd895af1d5a2705d8abff5e4ac03da0a1f3e
Dorfer Thomas Josef <1810653831@stud.fh-kufstein.ac.at>
2018-11-08 14:52:28 +0100

    Aufgabe4VariableAngelegtUndInKonstanteUmegwandelt

--- src/Uebung1_Package2/Aufgabe3.java
+++ src/Uebung1_Package2/Aufgabe3.java
@@ -3,12 +3,36 @@ package Uebung1_Package2;
 public class Aufgabe3 {
     public static void main(String[] args)
     {
+        //Aufgabe3
+        /*Erstellen Sie eine Variable mit der Bezeichnung name vom Typ String.
+        Weißen Sie der Variable Ihren eigenen Namen alsWert zu.
+        Geben Sie die Variable auf der Konsole aus.*/
         //Zwei Variablen mit unterschiedlichen Typen erstellt und jeder einen Wert hinzugefügt.
-        String a = "Hallo";
-        int b = 24;
 
+        String a = "Thomas Dorfer";
         System.out.println(a);
+
+
+        //Aufgabe4
+        /*Erstellen Sie eine Variable mit der Bezeichnung alter vom Typ int.
+        Weißen Sie der Variable Ihr Alter zu.
+        Geben Sie die Variable auf der Konsole aus.*/
+        //Variable fachhochschuleKufsteinTriol erstellt und "Fachhochschule Kufstein Tirol" zugewiesen
+
+        int b = 24;
         System.out.println(b);
+
+
+        //Aufgabe4
+        /*Erstellen Sie eine Variable des Typs String zur Speicherung von „Fachhochschule Kufstein
+        Tirol“.VerwendenSiedazudenVariablennamenfachhochschuleKufsteinTirol.*/
+        /*Anschließend den Variablen Namen auf  fhKufsteinTirol gekürzt*/
+        /*Zu guter letzt die Variable in eine Konstante umgewandelt.*/
+
+
+        final  String fhKufsteinTirol = "FH_KUFSTEIN_TIROL";
+        System.out.println(fhKufsteinTirol);
+
 
     }
 }
