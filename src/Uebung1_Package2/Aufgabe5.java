2fd255984f3b39c8355e4740d0f6f5b1b2e0e495
Dorfer Thomas Josef <1810653831@stud.fh-kufstein.ac.at>
2018-11-08 15:06:55 +0100

    Aufgabe5MittelsTastenkombisKonstatnenAngelegt

---
+++ src/Uebung1_Package2/Aufgabe5.java
@@ -0,0 +1,13 @@
+package Uebung1_Package2;
+
+    public class Aufgabe6 {
+
+        public static void main(String[] args)
+        {
+            //Wenn man den Anfangsbuchstaben eines Befehls eingibt, und danach die
+            //Tastenkombination "Strg+Leertaste" erscheint die Liste mit allen Befehlen.
+            final String c;
+
+            //Mit Strg+ALT+F kommt man genau auf die Position, wo sich ein Fehler befindet.
+        }
+}
